b2f535afe7a19012eb9cbbc673410bfa45e8923e
NikolayD <kolik885@ya.ru>
2022-08-17 22:35:13 +0700

    Программа к заданию №23

---
+++ 023/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа которая принимает на вход число N и выдает таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+
+Console.WriteLine("Введите число");
+int x = int.Parse(Console.ReadLine()!);
+int index = 1;
+
+while(x >= index)
+{
+   var result = Math.Pow(index, 3);
+   index++;
+   Console.Write(result + "  ");
+}
